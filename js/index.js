154484a4c37db1e8c4c5044b88255b722abeced0
Thanh Kim <kimhexi1998@gmail.com>
2020-09-21 19:52:11 +0700

    5 commit

--- js/index.js
+++ js/index.js
@@ -6,11 +6,21 @@ const googleSignIn = () => {
     // firebase.auth().languageCode = 'pt';
     firebase.auth().signInWithPopup(base_provider).then(function(result){
         console.log(result);
-        console.log("Success... Google account Linked 1");
+        console.log("Success... Google account Linked 2");
         var token = result.credential.accessToken;
         
         var user = result.user;
         console.log(token ,user);
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+              console.log("Đăng nhập thành công");
+              var user = firebase.auth().currentUser;
+              console.log(user);
+            } else {
+              // No user is signed in.
+            }
+          });
+
     }).catch(function(err){
         console.log(err);
         console.log("Failed to do");
